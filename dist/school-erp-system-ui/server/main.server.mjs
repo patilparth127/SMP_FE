0eba6d9823bae2e87c244439bc420c338c03bf0a
Parth <patilparth127@gmail.com>
2024-07-30 08:36:31 +0530

    commit do

--- dist/school-erp-system-ui/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-BVPSWYBG.mjs";import"./chunk-RXBUP6U6.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- dist/school-erpadmin-panel-ui/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-2FWNH62S.mjs";import"./chunk-7CNKQZAC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
